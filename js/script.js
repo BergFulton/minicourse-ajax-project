97b8bd04d93f718d7cc673bb8af601edb87a84dc
BergFulton <BergFulton@users.noreply.github.com>
2016-05-01 15:28:55 -0400

    Add loadStreetview

--- js/script.js
+++ js/script.js
@@ -13,8 +13,14 @@ function loadData() {
 
     // load streetview
 
-    // YOUR CODE GOES HERE!
-
+    var streetAd = $('#street').val();
+    console.log(streetAd);
+    var cityAd = $('#city').val();
+    console.log(cityAd);
+    var fullAd = streetAd + ',' + cityAd;
+    $greeting.text('Great, you want to live at' + fullAd + '!')
+    var streetviewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + fullAd + '';
+    $body.append('<img class="bgimg" src=" ' +streetviewURL + '">');
     return false;
 };
 
