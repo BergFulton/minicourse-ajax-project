86b612c014c1f77318d51174c0860d6abed5cbf9
BergFulton <BergFulton@users.noreply.github.com>
2016-05-04 07:32:35 -0400

    Fix: return: false was in wrong position, misplaced semicolons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+console.log('hi');
 
 function loadData() {
 
@@ -13,32 +14,33 @@ function loadData() {
 
     // load streetview
 
-    var streetAd = $('#street').val();
-    var cityAd = $('#city').val();
-    var fullAd = streetAd + ',' + cityAd;
-    $greeting.text('Great, you want to live at' + fullAd + '!');
+    console.log('here');
+
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+
+    $greeting.text('So, you want to live at ' + address + '?');
     
-    var streetviewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + fullAd + '';
+    var streetviewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src=" ' + streetviewURL + '">');
 
-//NYTimes Ajax request. Pretty please. 
-     var timesURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' +
-        cityAd + '&sort=newest&api-key=ea2b201fd46538db3668d93077d41282:8:75159936';
-         $.getJSON(timesURL, function(data){
-
-             $nytHeaderElem.text('New York Times Articles about ' + cityAd);
-
-             articles = data.response.docs;
-             for (var i = 0; i < articles.length; i++) {
-                 var article = articles[i];
-                 $nytElem.append('<li class="article">'+
-                     '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
-                     '<p>' + article.snippet + '</p>' +
-                     '</li>');
-             }
-                 return false;
-         });
-
+    //NYTimes Ajax request. Pretty please.
+    var timesURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' +
+    cityStr + '&sort=newest&api-key=ea2b201fd46538db3668d93077d41282:8:75159936';
+    $.getJSON(timesURL, function(data) {
+        $nytHeaderElem.text('New York Times Articles about ' + cityStr);
+        articles = data.response.docs;
+        console.log(articles);
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">' +
+                '<a href="' + article.web_url + '">' + article.headline.main + '</a>' +
+                '<p>' + article.snippet + '</p>' +
+                '</li>');
+        }
+    });
+    return false;
 }
 
 $('#form-container').submit(loadData);
